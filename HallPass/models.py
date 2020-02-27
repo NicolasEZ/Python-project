2ee0eed177b0a8ed38c993239193c4b664d43688
Nicolas Z <nicolas.e.zapata@gmail.com>
2020-02-27 12:32:52 -0800

    first commit

---
+++ HallPass/models.py
@@ -0,0 +1,59 @@
+from __future__ import unicode_literals
+from django.db import models
+import bcrypt
+import re
+
+class UserManager(models.Manager):
+	def registration_validator(self, postData):
+		errors = {}
+		EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+		if len(postData['fname']) < 1:
+			errors['first_name'] = "First name must be at least 1 character"
+		if len(postData['lname']) < 1:
+			errors['last_name'] = "Last name must be at least 1 character"
+		if not EMAIL_REGEX.match(postData['email']):
+			errors['email'] = "Invalid email address"
+		if len(postData['password']) < 8:
+			errors['password'] = "Password must be at least 8 characters"
+		if postData['password_confirm'] != postData['password']:
+			errors['password_confirm'] = "Password confirmation does not match password"
+		return errors
+
+	def login_validator(self, postData):
+		errors = {}
+		user = User.objects.filter(email=postData['email'])
+		if not user:
+			errors['email'] = "This email address does not exist in our records"
+		else:
+			user0 = user[0]
+			if not bcrypt.checkpw(postData['password'].encode(), user0.password.encode()):
+				errors['password'] = "Invalid password"
+		return errors
+
+class User(models.Model):
+	first_name = models.CharField(max_length=255)
+	last_name = models.CharField(max_length=255)
+	email = models.CharField(max_length=255)
+	password = models.CharField(max_length=255)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+	objects = UserManager()
+
+
+class CelebManager(models.Manager):
+    def celebvalidator(self, postData):
+        errors = {}
+        if len(postData['name']) < 3:
+            errors['name'] = "Name must be at least 3 characters"
+        if len(postData['comments']) < 3:
+            errors['comments'] = "Comments field must have at least 3 characters"
+        return errors
+
+class Celeb(models.Model):
+	ranking = models.PositiveSmallIntegerField()
+	photo = models.CharField(max_length=255)
+	name = models.CharField(max_length=255)
+	comments = models.TextField()
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+	objects = CelebManager()
---
+++ HallPass/views.py
@@ -0,0 +1,116 @@
+from django.shortcuts import render, redirect
+import bcrypt
+from django.contrib import messages
+from .models import User, Celeb
+from serpapi.google_search_results import GoogleSearchResults
+
+def index(request):
+    return render(request, "login.html")
+
+def register(request):
+	errors = User.objects.registration_validator(request.POST)
+	if len(errors) > 0:
+		for key, val in errors.items():
+			messages.error(request, val)
+		return redirect("/")
+
+	pw = request.POST['password']
+	pw_hash = bcrypt.hashpw(pw.encode(), bcrypt.gensalt()).decode()
+	new_user = User.objects.create(
+		first_name = request.POST['fname'],
+		last_name = request.POST['lname'],
+		email = request.POST['email'],
+		password = pw_hash
+	)
+	request.session['userid'] = new_user.id
+	return redirect("/myhallpass")
+
+def myhallpass(request):
+    if "userid" not in request.session:
+        return redirect("/")
+
+    context = {
+        "user": User.objects.filter(id=request.session['userid'])[0],
+        "celebs": Celeb.objects.all()
+    }
+    return render(request, "myhallpass.html", context)
+
+def login(request):
+    errors = User.objects.login_validator(request.POST)
+    if len(errors) > 0:
+        for key, val in errors.items():
+            messages.error(request, val)
+        return redirect("/")
+
+    user = User.objects.filter(email=request.POST['email'])[0]
+    request.session['userid'] = user.id
+    return redirect("/myhallpass")
+
+def logout(request):
+    del request.session["userid"]
+    return redirect("/")
+
+def newceleb(request):
+    if "userid" not in request.session:
+        return redirect("/")
+
+    context = {
+        "user": User.objects.filter(id=request.session['userid'])[0]
+    }
+    return render(request, "newceleb.html", context)
+
+def processnewceleb(request):
+    errors = Celeb.objects.celebvalidator(request.POST)
+    if len(errors) > 0:
+        for key, val in errors.items():
+            messages.error(request, val)
+        return redirect("/celebs/new")
+
+    celeb = Celeb.objects.create(
+        ranking=request.POST['ranking'],
+        photo=request.POST['photo'],
+        name=request.POST['name'],
+        comments=request.POST['comments'],
+    )
+    return redirect("/myhallpass")
+
+def destroy(request, id):
+    celeb = Celeb.objects.get(id=id)
+    celeb.delete()
+    return redirect("/myhallpass")
+
+def edit(request, id):
+    context = {
+        "celeb": Celeb.objects.get(id=id),
+        "user": User.objects.filter(id=request.session['userid'])[0]
+    }
+    return render(request, "edit.html", context)
+
+def update(request, id):
+    errors = Celeb.objects.celebvalidator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect(f'/celebs/edit/{id}')
+    else:
+        celeb = Celeb.objects.get(id=id)
+        celeb.ranking = request.POST['ranking']
+        celeb.photo = request.POST['photo']
+        celeb.name = request.POST['name']
+        celeb.comments = request.POST['comments']
+        celeb.save()
+        return redirect("/myhallpass")
+
+def test(request):
+    return render(request, "testing.html")
+
+def searchprocess(request):
+    client = GoogleSearchResults({"q": "coffe", "tbm": "isch"})
+    for image_result in client.get_json()['images_results']:
+        link = image_result["original"]
+        try:
+            print("link: " + link)
+            # wget.download(link, '.')
+        except:
+            pass
+
